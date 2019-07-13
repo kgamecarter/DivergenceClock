f1f591bb3754144aa7ca6f9145b5f92348925853
kgame <kgamecarter@gmail.com>
2019-07-13 22:16:24 +0800

    utf-8

--- 8051/Display.c
+++ 8051/Display.c
@@ -19,83 +19,83 @@ void watchDog()
 bool cmpPreState(byte i)
 {
     if (preState != i)
-	{
-	    preState = i;
-		for (i = 0; i < 8; i++)
-		    preStr[i] = nextStr[i];
-		return true;
-	}
-	return false;
+    {
+        preState = i;
+        for (i = 0; i < 8; i++)
+            preStr[i] = nextStr[i];
+        return true;
+    }
+    return false;
 }
 
-// ±½´yÅã¥Ü
+// æŽƒæé¡¯ç¤º
 void scan(byte* str)
 {
     byte i;
     for (i = 0; i < 8; i++)
     {
-	    watchDog();
-    	if (str[i] == OVERCHAR)
-    	    break;
+        watchDog();
+        if (str[i] == OVERCHAR)
+            break;
         if (str[i] == NONUM)
-    	{
+        {
             delayms(2);
             continue;
-    	}
+        }
         P1 = 1 << i;
 
-		P0 = str[i];
+        P0 = str[i];
         delayus(defaultLightness);
 
-		P0 = NONUM;
-    	P1 = 0;
+        P0 = NONUM;
+        P1 = 0;
         delayus(200 - defaultLightness);
     }
     P0 = NONUM;
-    delayms((8 - i) << 1); // delayº¡16ms
+    delayms((8 - i) << 1); // delayæ»¿16ms
 }
 
 void scanCrossfade(byte fadeCount)
 {
     byte i;
-	fadeCount = 100 - fadeCount;
-	if (fadeCount < crossfade) // »Ý­nFade
-	    fadeCount = defaultLightness * fadeCount / crossfade; //­pºâ©µ¿ð
-	else
-		fadeCount = 0xFF;
+    fadeCount = 100 - fadeCount;
+    if (fadeCount < crossfade) // éœ€è¦Fade
+        fadeCount = defaultLightness * fadeCount / crossfade; //è¨ˆç®—å»¶é²
+    else
+        fadeCount = 0xFF;
     for (i = 0; i < 8; i++)
     { 
-	    watchDog();
-    	if (preStr[i] == OVERCHAR)
-    	    break;
+        watchDog();
+        if (preStr[i] == OVERCHAR)
+            break;
         if (preStr[i] == NONUM)
-    	{
+        {
             delayms(2);
             continue;
-    	}
+        }
         P1 = 1 << i;
-		P0 = preStr[i];
-		if (fadeCount == 0xFF || preStr[i] == nextStr[i])
-		{
-	        delayus(defaultLightness);
-	
-			P0 = NONUM;
-	    	P1 = 0;
-	        delayus(200 - defaultLightness);
-		}
-		else // cross fade
-		{ 
-	        delayus(fadeCount);
-			P0 = nextStr[i];
-	        delayus(defaultLightness - fadeCount);
-	
-			P0 = NONUM;
-	    	P1 = 0;
-	        delayus(200 - defaultLightness);
-		}
+        P0 = preStr[i];
+        if (fadeCount == 0xFF || preStr[i] == nextStr[i])
+        {
+            delayus(defaultLightness);
+    
+            P0 = NONUM;
+            P1 = 0;
+            delayus(200 - defaultLightness);
+        }
+        else // cross fade
+        { 
+            delayus(fadeCount);
+            P0 = nextStr[i];
+            delayus(defaultLightness - fadeCount);
+    
+            P0 = NONUM;
+            P1 = 0;
+            delayus(200 - defaultLightness);
+        }
     }
     P0 = NONUM;
-    delayms((8 - i) << 1); // delayº¡16ms
+    delayms((8 - i) << 1); // delayæ»¿16ms
 }
 
 void scanLightness(byte* str, byte* lightness)
@@ -103,41 +103,41 @@ void scanLightness(byte* str, byte* lightness)
     byte i;
     for (i = 0; i < 8; i++)
     {
-	    watchDog();
-    	if (str[i] == OVERCHAR)
-    	    break;
+        watchDog();
+        if (str[i] == OVERCHAR)
+            break;
         if (str[i] == NONUM)
-    	{
+        {
             delayms(2);
             continue;
-    	}
+        }
 
         P1 = 1 << i;
 
-		P0 = str[i];
+        P0 = str[i];
         delayus(lightness[i]);
 
-		P0 = NONUM;
-    	P1 = 0;
+        P0 = NONUM;
+        P1 = 0;
         delayus(200 - lightness[i]);
     }
     P0 = NONUM;
-    delayms((8 - i) << 1); // delayº¡16ms
+    delayms((8 - i) << 1); // delayæ»¿16ms
 }
 
 void displayOff()
 {
-	P0 = NONUM;
-	P1 = 0;
+    P0 = NONUM;
+    P1 = 0;
 }
 
 void delayms(byte time)
 {
     while (time--)
-	{ 
+    { 
         delayus(100);
         watchDog();
-	}
+    }
 }
 
 void delayus(uint time)
